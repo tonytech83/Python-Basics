17b34b9029c3a8ab014a42919d1dc00faa5c74e3
Anton Petrov <a.petrov.83@gmail.com>
2022-05-21 17:55:52 +0300

    Added 11 Fruit shop and 12 Trade commissions

--- 05_Conditional_Statements_Advanced_Lab/11_fruit_shop.py
+++ 05_Conditional_Statements_Advanced_Lab/11_fruit_shop.py
@@ -1,1 +1,44 @@
-# TODO
+fruit = input()
+day_of_week = input()
+quantity = float(input())
+
+price = 0
+working_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+weekend = ["Saturday", "Sunday"]
+
+if day_of_week in working_days:
+    if fruit == "banana":
+        price = 2.5
+    elif fruit == "apple":
+        price = 1.2
+    elif fruit == "orange":
+        price = 0.85
+    elif fruit == "grapefruit":
+        price = 1.45
+    elif fruit == "kiwi":
+        price = 2.7
+    elif fruit == "pineapple":
+        price = 5.5
+    elif fruit == "grapes":
+        price = 3.85
+
+if day_of_week in weekend:
+    if fruit == "banana":
+        price = 2.7
+    elif fruit == "apple":
+        price = 1.25
+    elif fruit == "orange":
+        price = 0.9
+    elif fruit == "grapefruit":
+        price = 1.6
+    elif fruit == "kiwi":
+        price = 3
+    elif fruit == "pineapple":
+        price = 5.6
+    elif fruit == "grapes":
+        price = 4.2
+
+if price == 0:
+    print("error")
+else:
+    print(f"{(quantity * price):.2f}")
--- 05_Conditional_Statements_Advanced_Lab/12_trade_commissions.py
+++ 05_Conditional_Statements_Advanced_Lab/12_trade_commissions.py
@@ -1,1 +1,39 @@
-# TODO
+city = input()
+quantity_of_sells = float(input())
+
+commission = 0
+
+if city == "Sofia":
+    if 0 <= quantity_of_sells <= 500:
+        commission = 0.05
+    elif 500 < quantity_of_sells <= 1000:
+        commission = 0.07
+    elif 1000 < quantity_of_sells <= 10000:
+        commission = 0.08
+    else:
+        commission = 0.12
+
+if city == "Varna":
+    if 0 <= quantity_of_sells <= 500:
+        commission = 0.045
+    elif 500 < quantity_of_sells <= 1000:
+        commission = 0.075
+    elif 1000 < quantity_of_sells <= 10000:
+        commission = 0.1
+    else:
+        commission = 0.13
+
+if city == "Plovdiv":
+    if 0 <= quantity_of_sells <= 500:
+        commission = 0.055
+    elif 500 < quantity_of_sells <= 1000:
+        commission = 0.08
+    elif 1000 < quantity_of_sells <= 10000:
+        commission = 0.12
+    else:
+        commission = 0.145
+
+if commission == 0 or quantity_of_sells < 0:
+    print("error")
+else:
+    print(f"{(quantity_of_sells * commission):.2f}")
