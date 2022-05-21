e1ef75c58eb97fa8077315f8d8834c1241bdd8af
Anton Petrov <a.petrov.83@gmail.com>
2022-05-21 17:21:05 +0300

    Added 07 Working hours and 08 Cinema ticket

--- 05_Conditional_Statements_Advanced_Lab/07_working_hours.py
+++ 05_Conditional_Statements_Advanced_Lab/07_working_hours.py
@@ -1,1 +1,10 @@
-# TODO
+hour = int(input())
+day_of_week = input()
+
+if day_of_week == "Sunday":
+    print("closed")
+else:
+    if 10 <= hour <= 18:
+        print("open")
+    else:
+        print("closed")
--- 05_Conditional_Statements_Advanced_Lab/08_cinema_ticket.py
+++ 05_Conditional_Statements_Advanced_Lab/08_cinema_ticket.py
@@ -1,1 +1,12 @@
-# TODO
+day_of_week = input()
+
+price = 0
+
+if day_of_week == "Monday" or day_of_week == "Tuesday" or day_of_week == "Friday":
+    price = 12
+elif day_of_week == "Wednesday" or day_of_week == "Thursday":
+    price = 14
+else:
+    price = 16
+
+print(price)
