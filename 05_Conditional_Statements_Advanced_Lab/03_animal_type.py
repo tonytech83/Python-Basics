8fec57d6fae4c6430b81ad0f8351d84afd343523
Anton Petrov <a.petrov.83@gmail.com>
2022-05-21 16:51:40 +0300

    Added solution for 03 Animal type

--- 05_Conditional_Statements_Advanced_Lab/03_animal_type.py
+++ 05_Conditional_Statements_Advanced_Lab/03_animal_type.py
@@ -1,1 +1,9 @@
-# TODO
+animal = input()
+
+if animal == "dog":
+    print("mammal")
+elif animal == "crocodile" or animal == "tortoise" or animal == "snake":
+    print("reptile")
+else:
+    print("unknown")
+
