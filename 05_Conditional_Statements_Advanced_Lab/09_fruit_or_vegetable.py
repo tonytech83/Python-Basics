96d17988a034554107a14c1c34a3fb705ec3102e
Anton Petrov <a.petrov.83@gmail.com>
2022-05-21 17:29:11 +0300

    Added 09 Fruit or vegetable and 10 Invalid number

--- 05_Conditional_Statements_Advanced_Lab/09_fruit_or_vegetable.py
+++ 05_Conditional_Statements_Advanced_Lab/09_fruit_or_vegetable.py
@@ -1,1 +1,11 @@
-# TODO
+item = input()
+
+fruit = ["banana", "apple", "kiwi", "cherry", "lemon", "grapes"]
+vegetable = ["tomato", "cucumber", "pepper", "carrot"]
+
+if item in fruit:
+    print("fruit")
+elif item in vegetable:
+    print("vegetable")
+else:
+    print("unknown")
--- 05_Conditional_Statements_Advanced_Lab/10_invalid_number.py
+++ 05_Conditional_Statements_Advanced_Lab/10_invalid_number.py
@@ -1,1 +1,6 @@
-# TODO
+number = int(input())
+
+if 100 <= number <= 200 or number == 0:
+    pass
+else:
+    print("invalid")
