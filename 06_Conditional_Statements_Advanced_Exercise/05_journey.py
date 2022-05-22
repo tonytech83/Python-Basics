6fb68582831d7a873f8078f56218893bf74995cb
Anton Petrov <a.petrov.83@gmail.com>
2022-05-22 16:06:34 +0300

    Added 05 Journey and 07 Hotel room

--- 06_Conditional_Statements_Advanced_Exercise/05_journey.py
+++ 06_Conditional_Statements_Advanced_Exercise/05_journey.py
@@ -1,1 +1,29 @@
-# TODO
+budget = float(input())
+season = input()
+
+price = 0
+where = "Hotel"
+destination = ""
+
+if budget <= 100:
+    destination = "Bulgaria"
+    if season == "summer":
+        price = budget * 0.3
+        where = "Camp"
+    elif season == "winter":
+        price = budget * 0.7
+
+elif budget <= 1000:
+    destination = "Balkans"
+    if season == "summer":
+        price = budget * 0.4
+        where = "Camp"
+    elif season == "winter":
+        price = budget * 0.8
+
+else:
+    destination = "Europe"
+    price = budget * 0.9
+
+print(f"Somewhere in {destination}")
+print(f"{where} - {price:.2f}")
--- 06_Conditional_Statements_Advanced_Exercise/07_hotel_room.py
+++ 06_Conditional_Statements_Advanced_Exercise/07_hotel_room.py
@@ -1,1 +1,34 @@
-# TODO
+month = input()
+nights_count = int(input())
+
+range_1 = ["May", "October"]
+range_2 = ["June", "September"]
+range_3 = ["July", "August"]
+
+price_apartment = 0
+price_studio = 0
+
+if month in range_1:
+    price_studio = nights_count * 50
+    price_apartment = nights_count * 65
+    if 7 < nights_count <= 14:
+        price_studio *= 0.95
+    elif nights_count > 14:
+        price_studio *= 0.7
+        price_apartment *= 0.9
+
+elif month in range_2:
+    price_studio = nights_count * 75.2
+    price_apartment = nights_count * 68.7
+    if nights_count > 14:
+        price_studio *= 0.8
+        price_apartment *= 0.9
+
+elif month in range_3:
+    price_studio = nights_count * 76
+    price_apartment = nights_count * 77
+    if nights_count > 14:
+        price_apartment *= 0.9
+
+print(f"Apartment: {price_apartment:.2f} lv.")
+print(f"Studio: {price_studio:.2f} lv.")
