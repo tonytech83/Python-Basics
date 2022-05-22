7988ecc45d7c8e5ef3c6e72b767c45ab65186902
Anton Petrov <a.petrov.83@gmail.com>
2022-05-22 15:35:07 +0300

    Added 03 New house and 04 Fishing boat

--- 06_Conditional_Statements_Advanced_Exercise/03_new_house.py
+++ 06_Conditional_Statements_Advanced_Exercise/03_new_house.py
@@ -1,1 +1,34 @@
-# TODO
+flower_type = input()
+count = int(input())
+budget = int(input())
+
+price = 0
+
+if flower_type == "Roses":
+    price = 5
+    if count > 80:
+        price *= 0.9
+elif flower_type == "Dahlias":
+    price = 3.8
+    if count > 90:
+        price *= 0.85
+elif flower_type == "Tulips":
+    price = 2.8
+    if count > 80:
+        price *= 0.85
+elif flower_type == "Narcissus":
+    price = 3
+    if count < 120:
+        price *= 1.15
+else:  # check for Gladiolus
+    price = 2.5
+    if count < 80:
+        price *= 1.2
+
+total_price = price * count
+diff = abs(budget - total_price)
+
+if budget >= total_price:
+    print(f"Hey, you have a great garden with {count} {flower_type} and {diff:.2f} leva left.")
+else:
+    print(f"Not enough money, you need {diff:.2f} leva more.")
--- 06_Conditional_Statements_Advanced_Exercise/04_fishing_boat.py
+++ 06_Conditional_Statements_Advanced_Exercise/04_fishing_boat.py
@@ -1,1 +1,33 @@
-# TODO
+budget = int(input())
+season = input()
+fishermen_count = int(input())
+
+boat_rent = 0
+final_price = 0
+
+# check the rent season to determine the price
+if season == "Spring":
+    boat_rent = 3000
+elif season == "Summer" or season == "Autumn":
+    boat_rent = 4200
+else:  # check for Winter
+    boat_rent = 2600
+
+# check the fisherman count to determine the discount
+if fishermen_count <= 6:
+    final_price = boat_rent * 0.9
+elif 7 <= fishermen_count <= 11:
+    final_price = boat_rent * 0.85
+else:  # check when fishermen are more than 11
+    final_price = boat_rent * 0.75
+
+# check if fishermen are even
+if fishermen_count % 2 == 0 and season != "Autumn":
+    final_price *= 0.95
+
+diff = abs(budget - final_price)
+
+if budget >= final_price:
+    print(f"Yes! You have {diff:.2f} leva left.")
+else:
+    print(f"Not enough money! You need {diff:.2f} leva.")
