d9a7bc2e33bc061d3972878c0c4aff4dc3b7296a
Anton Petrov <a.petrov.83@gmail.com>
2022-05-22 19:47:50 +0300

    Added 08 On time for the exam and 09 Ski trip

--- 06_Conditional_Statements_Advanced_Exercise/08_on_time_for_the_exam.py
+++ 06_Conditional_Statements_Advanced_Exercise/08_on_time_for_the_exam.py
@@ -1,1 +1,38 @@
-# TODO
+exam_hours = int(input())
+exam_minutes = int(input())
+arrival_hours = int(input())
+arrival_minutes = int(input())
+
+# convert hours in minutes and calculate time
+exam_time_to_minutes = exam_hours * 60 + exam_minutes
+arrival_time_to_minutes = arrival_hours * 60 + arrival_minutes
+minutes_diff = abs(exam_time_to_minutes - arrival_time_to_minutes)
+
+hours = minutes_diff // 60
+minutes = minutes_diff % 60
+
+# check the status of arrival
+if 0 <= (exam_time_to_minutes - arrival_time_to_minutes) <= 30:
+    print("On time")
+    if minutes_diff != 0:
+        print(f"{minutes_diff} minutes before the start")
+
+elif arrival_time_to_minutes < exam_time_to_minutes:
+    print("Early")
+    if minutes_diff < 60:
+        print(f"{minutes_diff} minutes before the start")
+    else:
+        if minutes < 10:
+            print(f"{hours}:0{minutes} hours before the start")
+        else:
+            print(f"{hours}:{minutes} hours before the start")
+
+elif arrival_time_to_minutes > exam_time_to_minutes:
+    print("Late")
+    if minutes_diff < 60:
+        print(f"{minutes_diff} minutes after the start")
+    else:
+        if minutes < 10:
+            print(f"{hours}:0{minutes} hours after the start")
+        else:
+            print(f"{hours}:{minutes} hours after the start")
--- 06_Conditional_Statements_Advanced_Exercise/09_ski_trip.py
+++ 06_Conditional_Statements_Advanced_Exercise/09_ski_trip.py
@@ -1,1 +1,34 @@
-# TODO
+nights_count = int(input()) - 1
+room_type = input()
+rating = input()
+
+discount = 0
+price = 0
+
+if room_type == "room for one person":
+    price = nights_count * 18
+
+elif room_type == "apartment":
+    if nights_count < 10:
+        discount = 0.7
+    elif 10 <= nights_count <= 15:
+        discount = 0.65
+    else:
+        discount = 0.5
+    price = nights_count * 25 * discount
+
+elif room_type == "president apartment":
+    if nights_count < 10:
+        discount = 0.9
+    elif 10 <= nights_count <= 15:
+        discount = 0.85
+    else:
+        discount = 0.8
+    price = nights_count * 35 * discount
+
+if rating == "positive":
+    price *= 1.25
+else:
+    price *= 0.90
+
+print(f"{price:.2f}")
