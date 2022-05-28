010a94174fef56d6b212e733b229075efe7880fa
Anton Petrov <a.petrov.83@gmail.com>
2022-05-28 16:18:39 +0300

    Added exercise 10 Odd and Even sums

---
+++ 07_For_Loop_Lab/10_odd_even_sum.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+even_sum = 0
+odd_sum = 0
+
+for position in range(1, n + 1):
+    current_num = int(input())
+    if position % 2 == 0:
+        even_sum += current_num
+    else:
+        odd_sum += current_num
+
+if even_sum == odd_sum:
+    print("Yes")
+    print(f"Sum = {even_sum}")
+else:
+    diff = abs(even_sum - odd_sum)
+    print("No")
+    print(f"Diff = {diff}")
