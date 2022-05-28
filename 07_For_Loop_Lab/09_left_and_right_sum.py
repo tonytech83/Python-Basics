4b1325df718ca9ed04adf07354c9a8a6fa534bcf
Anton Petrov <a.petrov.83@gmail.com>
2022-05-28 16:13:50 +0300

    Added exercise 09 Left and Right sum

--- 07_For_Loop_Lab/09_left_and_right_sum.py
+++ 07_For_Loop_Lab/09_left_and_right_sum.py
@@ -1,11 +1,11 @@
-n = int(input())
+number_from_console = int(input())
 
 left_sum = 0
 right_sum = 0
 
-for num in range(n * 2):
+for num in range(number_from_console * 2):
     current_num = int(input())
-    if num < n:
+    if num < number_from_console:
         left_sum += current_num
     else:
         right_sum += current_num
