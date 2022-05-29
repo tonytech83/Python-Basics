86bde2fdc976e91304378dfd3c31ea41627aeb61
Anton Petrov <a.petrov.83@gmail.com>
2022-05-29 10:49:21 +0300

    Changed exam 02 Half sum element - made changes on some variables

--- 08_For_Loop_Exercise/02_half_sum_element.py
+++ 08_For_Loop_Exercise/02_half_sum_element.py
@@ -3,20 +3,20 @@ import sys
 n = int(input())
 
 max_num = -sys.maxsize
-sum_of_small_nums = 0
+sum_of_all = 0
 
 for num in range(n):
     current_mun = int(input())
-    sum_of_small_nums += current_mun
+    sum_of_all += current_mun
     if current_mun > max_num:
         max_num = current_mun
 
-sum_of_small_nums -= max_num
+sum_of_small_muns = sum_of_all - max_num
 
-if max_num == sum_of_small_nums:
+if max_num == sum_of_small_muns:
     print("Yes")
     print(f"Sum = {max_num}")
 else:
-    diff = abs(max_num - sum_of_small_nums)
+    diff = abs(max_num - sum_of_small_muns)
     print("No")
     print(f"Diff = {diff}")
