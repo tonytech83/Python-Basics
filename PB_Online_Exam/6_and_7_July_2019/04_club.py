feb60539690f35ece5b78b8f381b8aa1309b7028
Anton Petrov <a.petrov.83@gmail.com>
2022-06-01 08:49:12 +0300

    Added exam 04 Club with 100/100 in judge

--- PB_Online_Exam/6_and_7_July_2019/04_club.py
+++ PB_Online_Exam/6_and_7_July_2019/04_club.py
@@ -2,8 +2,9 @@ target_profit = float(input())
 
 bill = 0
 profit = 0
+cocktail = ""
 
-while target_profit >= profit:
+while target_profit > profit:
     cocktail = input()
     if cocktail == "Party!":
         break
@@ -15,14 +16,11 @@ while target_profit >= profit:
     else:
         profit += bill
 
-diff = abs(target_profit - profit)
+diff = target_profit - profit
 
-if cocktail == "Party!":
+if diff > 0 and cocktail == "Party!":
     print(f"We need {diff:.2f} leva more.")
     print(f"Club income - {profit:.2f} leva.")
-
-else:
+elif diff <= 0:
     print(f"Target acquired.")
     print(f"Club income - {profit:.2f} leva.")
-
-# TODO 90/100
