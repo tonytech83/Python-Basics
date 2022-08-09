ac9a0515c626826097d7cb6caaf012f0fef16fe0
Anton Petrov <a.petrov.83@gmail.com>
2022-08-09 16:16:54 +0300

    Added exam "01. Birthday Party" from "Programming Basics Online Exam - 28 and 29 March 2020"

---
+++ PB_Online_Exam/28_and_29_March_2020/01_birthday_party.py
@@ -0,0 +1,9 @@
+hall_rent = float(input())
+
+cake_price = hall_rent * 0.2
+drinks_price = cake_price * 0.55
+animator_price = hall_rent / 3
+
+total = hall_rent + cake_price + drinks_price + animator_price
+
+print(total)
