a8357e65dfe5a83e218ca79e4a1d316c8749200f
Anton Petrov <a.petrov.83@gmail.com>
2022-08-23 20:51:06 +0300

    Added exam "10. Profit" from "Nested Loops - More Exercises" - added changes

--- PB_More_Exercises/07_Nested_Loops__More_Exercises/10_profit.py
+++ PB_More_Exercises/07_Nested_Loops__More_Exercises/10_profit.py
@@ -8,5 +8,3 @@ for one in range(0, one_amount + 1):
         for five in range(0, five_amount + 1):
             if (one * 1 + two * 2 + five * 5) == amount:
                 print(f'{one} * 1 lv. + {two} * 2 lv. + {five} * 5 lv. = {amount} lv.')
-
-
